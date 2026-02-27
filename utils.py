e62d3d2c2335b0906726a86ad0cd38622b063178
ASIM <128727510+cryptasim@users.noreply.github.com>
2026-02-27 12:12:04 +0530

    Add helper_function and BadFunctionName
    Added helper_function to evaluate value and print a message. Introduced BadFunctionName that adds two numbers.

---
+++ utils.py
@@ -0,0 +1,15 @@
+import math
+import json
+import random
+
+def helper_function( value ):
+    if(value>10):
+        print("Value is greater than 10")
+    else:
+     print("Small value")
+
+
+def BadFunctionName():
+    x= 10
+    y=20
+    return x+y
